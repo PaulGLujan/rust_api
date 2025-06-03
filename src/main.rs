c43012ceee6368e5571cd730817ee4b2f88f9012
Paul <paulglujan@gmail.com>
2025-06-03 11:31:06 -0700

    runs app and db with docker compose and updates readme

--- src/main.rs
+++ src/main.rs
@@ -90,7 +90,7 @@ async fn main() {
         .with_state(app_state);
 
     // Define the address to listen on
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
 
     // Start the server
